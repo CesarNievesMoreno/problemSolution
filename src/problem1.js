0da8ca397bdb1fc5c28ebda34cd28d8bcb0a626c
Cesar Nieves <cesarnieves6@gmail.com>
2023-07-24 15:55:36 -0600

    Description and complexity

--- src/problem1.js
+++ src/problem1.js
@@ -1,7 +1,18 @@
+// Problem 1
+// The time and space complexity are O(N) it needs to iterate the array and validate all position one time
+// The size of the numList set depends on the number of elements found in the nums array.
 nums = (list) =>{
-    let result = []
-    for (let i = 0; i < list.length; i++) {
-     if(!list.find(e=> e === i+1)) result.push(i+1)
+    //create a set of the numbers provided in the list array
+    //using the set we will remove the duplicate numbers 
+    let numList = new Set(list)
+    //declare a array where the result numbers will be saved
+    let result =[]
+    //with this iteration we will have the range of numbers that we are looking for 
+    //where each value of i will be the value of the possible missing number
+    for (let i = 1; i <= list.length; i++) {
+    //for each iteration it check if our set of numbers has the value of i, if it does not
+    //exist in the set the value of i is saved in the result array
+     if(!numList.has(i)) result.push(i)
     }
     return result
 }
--- src/problem2.js
+++ src/problem2.js
@@ -1,15 +1,26 @@
-list = (nums,target)=>{
-    const res={}
+/* Problem 1
+    -The time and space complexity are O(N) it needs to iterate the array and validate all position one time
+    -The size of the res Map depends on the number of elements found in the nums array.*/
+    list = (nums,target)=>{
+    //create a map to save the num values to then validate if it contains its complement
+    const res= new Map()
+    //iterate by the nums length
     for(ind = 0; ind < nums.length; ind++){
-
+        // for each iteration get the number in the position i of the nums array 
+        //with the const num obtained calculate the complement 
         const num = nums[ind]
         const complement = target - num
 
-        if(res.hasOwnProperty(complement)){
-            return [res[complement], ind]
+        /*then validate if the res map has a propertie named like the complement result 
+          if it is contains it, it will retur the position of the complemtent and the current index(ind)
+          the values that will contain the res Map are the property name as
+          the complement value and its value will be the position of nums array where the value is*/
+        if(res.has(complement)){
+            return [res.get(complement), ind]
         }
-
-        res[num] = ind
+        //if res has not the complemt then we save the num  and the current index to later
+        //  be able to be validated as a complement
+        res.set(num, ind)
     }
     return []
 }
